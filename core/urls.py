128d87a6142643da7e1f866ae7f9dfcb46dfc838
Rastra17 <57227732+Rastra17@users.noreply.github.com>
2022-01-25 16:51:11 +0545

    some additions

--- core/urls.py
+++ core/urls.py
@@ -10,5 +10,5 @@ app_name='core'
 urlpatterns = [
     path('', home, name="home"),
     path('checkout/', checkout, name="checkout"),
-    path('product/', products, name="products")
+    path('products/', products, name="products"),
 ]
