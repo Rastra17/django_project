ff0724e2ab739c7abb386a80bcccd9bf75172af6
Rastra17 <57227732+Rastra17@users.noreply.github.com>
2022-01-26 19:44:07 +0545

    some changes and additions

--- core/views.py
+++ core/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView, DetailView
 from django.shortcuts import redirect
@@ -37,14 +38,18 @@ def add_to_cart(request, slug):
         if order.items.filter(item__slug=item.slug).exists():
             order_item.quantity += 1
             order_item.save()
+            messages.info(request, "The item quantity was updated")
         else:
             order.items.add(order_item)
+            messages.info(request, "This item was added to your cart")
+            return redirect("core:product", slug=slug)
     else:
         ordered_date = timezone.now()
         order = Order.objects.create(
             user=request.user, ordered_date=ordered_date)
         order.items.add(order_item)
-    return redirect("core:product", slug=slug)
+        messages.info(request, "This item was added to your cart")
+        return redirect("core:product", slug=slug)
 
 def remove_from_cart(request, slug):
     item = get_object_or_404(Item, slug=slug)
@@ -62,9 +67,12 @@ def remove_from_cart(request, slug):
                 ordered=False
             )[0]
             order.items.remove(order_item)
+            messages.info(request, "This item was removed from your cart")
+            return redirect("core:product", slug=slug)
         else:
+            messages.info(request, "This item was not in your cart")
             return redirect("core:product", slug=slug)
     else:
-        # Add a message saying the user doesn't have an order
+        messages.info(request, "You do not have an active order")
         return redirect("core:product", slug=slug)
     return redirect("core:product", slug=slug)
