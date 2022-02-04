c92b5b85610b782dd661db2facd4ebcd3a264da0
Rastra17 <57227732+Rastra17@users.noreply.github.com>
2022-02-04 17:08:28 +0545

    Making signup and creating a filter in products

--- store/views.py
+++ store/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 def index(request):
-    products = Product.get_all_products()
-    return render(request, 'index.html', {'products':products})
+    products = None
+    categories = Category.get_all_categories()
+    categoryId = request.GET.get('category')
+    if categoryId:
+        products = Product.get_all_products_by_categoryid(categoryId)
+    else:
+        products = Product.get_all_products()
+    data = {}
+    data['products'] = products
+    data['categories'] = categories
+    return render(request, 'index.html', data)
+
+def signup(request):
+    return render(request, 'signup.html')
